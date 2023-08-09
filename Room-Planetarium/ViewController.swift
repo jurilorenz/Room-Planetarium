74234eea25fbd865235771df417291c513cc7d61
jurilorenz <114692921+jurilorenz@users.noreply.github.com>
2023-08-09 19:03:32 +0200

    Added switching between different planet types and update the displayed planet's surface and radius when the screen is touched.

--- Room-Planetarium/ViewController.swift
+++ Room-Planetarium/ViewController.swift
@@ -13,30 +13,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var currentPlanet: PlanetType = .earth
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
         sceneView.delegate = self
         
-        let sphere = SCNSphere(radius: 0.2)
-        
-        let material = SCNMaterial()
-        
-        material.diffuse.contents = UIImage(named: "art.scnassets/venus_surface.jpg")
-        
-        sphere.materials = [material]
-        
-        let node = SCNNode()
-        
-        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
-        
-        node.geometry = sphere
-        
-        sceneView.scene.rootNode.addChildNode(node)
-        
         sceneView.autoenablesDefaultLighting = true
-        
+        updateDisplayedPlanet()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -55,4 +41,68 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        // Cycle through planet types and update displayed planet
+        switch currentPlanet {
+        case .mercury: currentPlanet = .venus
+        case .venus: currentPlanet = .earth
+        case .earth: currentPlanet = .moon
+        case .moon: currentPlanet = .mars
+        case .mars: currentPlanet = .jupiter
+        case .jupiter: currentPlanet = .neptune
+        case .neptune: currentPlanet = .uranus
+        case .uranus: currentPlanet = .mercury
+        }
+        
+        updateDisplayedPlanet()
+    }
+    
+    func updateDisplayedPlanet() {
+        let sphere = SCNSphere(radius: planetRadius(for: currentPlanet))
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named: planetSurface(for: currentPlanet))
+        sphere.materials = [material]
+
+        let node = SCNNode()
+        node.position = SCNVector3(x: 0, y: 0.1, z: -100)
+        node.geometry = sphere
+        
+        // Remove the previous planet node
+        sceneView.scene.rootNode.childNodes.forEach { $0.removeFromParentNode() }
+        
+        sceneView.scene.rootNode.addChildNode(node)
+    }
+    
+    func planetRadius(for planetType: PlanetType) -> Double {
+        switch planetType {
+        case .mercury: return RadiusConstants.mercuryRadius
+        case .venus: return RadiusConstants.venusRadius
+        case .earth: return RadiusConstants.earthRadius
+        case .moon: return RadiusConstants.moonRadius
+        case .mars: return RadiusConstants.marsRadius
+        case .jupiter: return RadiusConstants.jupiterRadius
+        case .neptune: return RadiusConstants.neptuneRadius
+        case .uranus: return RadiusConstants.uranusRadius
+        }
+    }
+
+    func planetSurface(for planetType: PlanetType) -> String {
+        switch planetType {
+        case .mercury: return SurfaceConstants.mercurySurface
+        case .venus: return SurfaceConstants.venusSurface
+        case .earth: return SurfaceConstants.earthSurface
+        case .moon: return SurfaceConstants.moonSurface
+        case .mars: return SurfaceConstants.marsSurface
+        case .jupiter: return SurfaceConstants.jupiterSurface
+        case .neptune: return SurfaceConstants.neptuneSurface
+        case .uranus: return SurfaceConstants.uranusSurface
+        }
+    }
+}
+
+enum PlanetType {
+    case mercury, venus, earth, moon, mars, jupiter, neptune, uranus
 }
---
+++ Supporting Files/Constants.swift
@@ -0,0 +1,31 @@
+//
+//  Constants.swift
+//  Room-Planetarium
+//
+//  Created by Juri Lorenz on 09.08.23.
+//
+
+import Foundation
+
+struct RadiusConstants {
+    static let mercuryRadius: Double = 2.439
+    static let venusRadius: Double = 6.051
+    static let earthRadius: Double = 6.371
+    static let moonRadius: Double = 1.737
+    static let marsRadius: Double = 3.389
+    static let jupiterRadius: Double = 69.911
+    static let neptuneRadius: Double = 24.622
+    static let uranusRadius: Double = 25.362
+    // in kilometers / 1000
+}
+
+struct SurfaceConstants {
+    static let mercurySurface: String = "art.scnassets/mercury.jpg"
+    static let venusSurface: String = "art.scnassets/venus.jpg"
+    static let earthSurface: String = "art.scnassets/earth.jpg"
+    static let moonSurface: String = "art.scnassets/moon.jpg"
+    static let marsSurface: String = "art.scnassets/mars.jpg"
+    static let jupiterSurface: String = "art.scnassets/jupiter.jpg"
+    static let neptuneSurface: String = "art.scnassets/neptune.jpg"
+    static let uranusSurface: String = "art.scnassets/uranus.jpg"
+}
